ac6b6780c5b9468047142d8dbae736322cf65e99
Clement Lakhal <clementlakhal@gmail.com>
2019-09-08 06:10:26 -0400

    Update backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const web3 = new Web3(
 );
 
 const abi = [{"constant":true,"inputs":[],"name":"backend","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"_vendor","type":"string"},{"internalType":"string","name":"_product","type":"string"}],"name":"backendPurchaseProduct","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"vendors","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"","type":"string"}],"name":"vendorNames","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"address","name":"_vendorContract","type":"address"}],"name":"addVendor","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"location","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"_vendor","type":"string"},{"internalType":"string","name":"_product","type":"string"}],"name":"purchaseProduct","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"vendorContracts","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_location","type":"string"},{"internalType":"address","name":"_backend","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"_vendor","type":"string"},{"indexed":false,"internalType":"string","name":"_product","type":"string"},{"indexed":false,"internalType":"uint256","name":"_timestamp","type":"uint256"}],"name":"ProductPurchased","type":"event"}];
-const contractAddress = '0x984c682e4cd0dfC8c5B1ffd2fc7900d18F26d550';
+const contractAddress = '0xe0a439d34F3C1ccf83a609202018AA0900Ba3fA9';
 
 const contract = new web3.eth.Contract(abi, contractAddress);
 
@@ -26,7 +26,7 @@ function pushProduct(servo) {
 
   servo.ccw(1);
 
-  setTimeout(stopServo, 1000, servo);
+  setTimeout(stopServo, 2000, servo);
 }
 
 board.on('ready', () => {
@@ -49,9 +49,9 @@ board.on('ready', () => {
     if (!err) {
       console.log(event);
 
-      console.log('Pushing product:', event.returnValues.product);
+      console.log('Pushing product:', event.returnValues._product);
 
-      // pushProduct(servos[4]);
+      pushProduct(servos[0]);
     }
   });
 });
